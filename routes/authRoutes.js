1f42898e2c362a3bb43e1d53589594d95a6fe919
Ilham Ahmad Reza Pahlevi <119603282+Ilevi2@users.noreply.github.com>
2024-11-30 11:02:48 +0700

    Add files via upload

---
+++ routes/authRoutes.js
@@ -0,0 +1,64 @@
+const authHandler = require('../handlers/authHandler');
+
+const authRoutes = [
+  {
+    method: 'POST',
+    path: '/register',
+    handler: authHandler.registerUser,
+  },
+  {
+    method: 'POST',
+    path: '/login',
+    handler: authHandler.loginUser,
+  },
+  {
+    method: 'POST',
+    path: '/verify/{userid}',
+    handler: authHandler.verifyToken,
+  },
+
+  {
+    method: 'GET',
+    path: '/product',
+    handler: authHandler.getAllProducts,
+  },
+  {
+    method: 'GET',
+    path: '/product/{productId}',
+    handler: authHandler.getProductById,
+  },
+  {
+  
+    method: 'GET',
+    path: '/product/category/{category}',
+    handler: authHandler.getProductsByCategory,
+  
+  },
+  {
+    method: 'GET',
+    path: '/product/seller/{sellerId}',
+    handler: authHandler.getProductsBySeller,
+  },
+  {
+    method: 'GET',
+    path: '/product/search', 
+    handler: authHandler.searchProducts,
+  },
+  {
+    method: 'POST',
+    path: '/product',
+    handler: authHandler.addProduct,
+  },
+  {
+    method: 'PUT',
+    path: '/product/{productId}',
+    handler: authHandler.updateProduct,
+  },
+  {
+    method: 'DELETE',
+    path: '/product/{productId}',
+    handler: authHandler.deleteProduct,
+  },
+];
+
+module.exports = authRoutes;
